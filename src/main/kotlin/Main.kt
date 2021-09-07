4839b548508180739f6a7fe4cf0ef289c7956059
Risda Tamam <risda123tamam@gmail.com>
2021-09-07 19:31:00 +0700

    impelementasi Single Comment pada Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,13 @@
+
 fun main() {
     car()
 }
 
+// Merupakan kelas car
 fun car() {
     val carList = arrayListOf("BMW", "Mercedes Benz", "Lamborgini", "Ferrari", "Mazda")
+
+    // Perulangan menggunakan forEach
     carList.forEach {
         println("Aku memiliki mobil: $it")
     }
